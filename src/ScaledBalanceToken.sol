7930e7f2047fd017d532d9b0ada5d041334bd33b
Dr Laurence E. Day <laurence.e.day@outlook.com>
2022-07-28 13:24:19 +0100

    Moved some ERC20 funcs into ScaledBalanceToken

--- src/ScaledBalanceToken.sol
+++ src/ScaledBalanceToken.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import { DefaultVaultState, VaultState, VaultStateCoder } from "./types/VaultStateCoder.sol";
 import "./libraries/Math.sol";
+import "./libraries/LowGasSafeMath.sol";
 
 uint256 constant SecondsIn365Days = 31536000;
 uint256 constant RayBipsNumerator = 1e22;
@@ -10,9 +11,13 @@ uint256 constant RayBipsNumerator = 1e22;
 abstract contract ScaledBalanceToken {
   using VaultStateCoder for VaultState;
   using Math for uint256;
+  using LowGasSafeMath for uint256;
 
   VaultState internal _state;
   mapping(address => uint256) public scaledBalanceOf;
+
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
 
   constructor(int256 _annualInterestBips) {
     _state = DefaultVaultState.setInitialState(_annualInterestBips, RayOne, block.timestamp);
@@ -22,7 +27,7 @@ abstract contract ScaledBalanceToken {
                           External Getters
   //////////////////////////////////////////////////////////////*/
 
-  function balanceOf(address account) external view returns (uint256) {
+  function balanceOf(address account) public view returns (uint256) {
     (uint256 scaleFactor, ) = _getCurrentScaleFactor(_state);
     return scaledBalanceOf[account].rayMul(scaleFactor);
   }
@@ -116,6 +121,39 @@ abstract contract ScaledBalanceToken {
   /*//////////////////////////////////////////////////////////////
                             ERC20 Actions
   //////////////////////////////////////////////////////////////*/
+  
+  mapping(address => mapping(address => uint256)) public allowance;
+
+  function _approve(
+    address owner,
+    address spender,
+    uint256 amount
+  ) internal {
+    require(owner != address(0), "ERC20: approve from the zero address");
+    require(spender != address(0), "ERC20: approve to the zero address");
+
+    allowance[owner][spender] = amount;
+    emit Approval(owner, spender, amount);
+  }
+
+  function approve(address spender, uint256 amount) external returns (bool) {
+    _approve(msg.sender, spender, amount);
+    return true;
+  }
+
+  function increaseAllowance(address spender, uint256 addedValue) external returns (bool) {
+    _approve(msg.sender, spender, allowance[msg.sender][spender].add(addedValue));
+    return true;
+  }
+
+  function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool) {
+    _approve(
+      msg.sender,
+      spender,
+      allowance[msg.sender][spender].sub(subtractedValue, "ERC20: decreased allowance below zero")
+    );
+    return true;
+  }
 
   function _beforeMint(
     address to,
