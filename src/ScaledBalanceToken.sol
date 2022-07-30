02ca67a4b76032137b5319b12b6498a27a24be7c
Dr Laurence E. Day <laurence.e.day@outlook.com>
2022-07-30 22:18:04 +0100

    Add transferFrom to ScaledBalanceToken

--- src/ScaledBalanceToken.sol
+++ src/ScaledBalanceToken.sol
@@ -174,6 +174,20 @@ abstract contract ScaledBalanceToken {
     }
   }
 
+  function transferFrom(
+    address sender,
+    address recipient,
+    uint256 amount
+  ) external returns (bool) {
+    _transfer(sender, recipient, amount);
+    _approve(
+      sender,
+      msg.sender,
+      allowance[sender][msg.sender].sub(amount, "ERC20: transfer amount exceeds allowance")
+    );
+    return true;
+  }
+
   function transfer(address recipient, uint256 amount) external returns (bool) {
     _transfer(msg.sender, recipient, amount);
     return true;
