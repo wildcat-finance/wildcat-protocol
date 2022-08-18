0055270fe4bb5113c0527a5ee599c7d134cb1c7a
d1ll0n <dillonkellar@protonmail.com>
2022-08-18 15:23:47 -0500

    Update tests

--- test/WMVaultFactory.t.sol
+++ test/WMVaultFactory.t.sol
@@ -13,8 +13,18 @@ import "src/WMPermissions.sol";
 import "src/WMRegistry.sol";
 import "src/WMVaultFactory.sol";
 
+uint256 constant DefaultMaximumSupply = 100_000e18;
+int256 constant DefaultAPRBips = 500;
+uint256 constant DefaultCollateralizationRatio = 90;
+
 contract VaultFactoryTest is Test {
     using stdStorage for StdStorage;
+    using Math for uint256;
+    using Math for int256;
+
+    int256 internal immutable DefaultInterestPerSecondRay;
+
+    bytes32 public immutable VaultInitCodeHash = keccak256(type(WMVault).creationCode);
 
     address internal wintermute = address(0x69);
 
@@ -35,6 +45,11 @@ contract VaultFactoryTest is Test {
       DAI = IERC20(address(new TestERC20("Dai Stablecoin", "DAI", 18)));
       WETH = IERC20(address(new TestERC20("Wrapped Ether", "WETH", 18)));
       WBTC = IERC20(address(new TestERC20("Wrapped Bitcoin", "WBTC", 8)));
+      DefaultInterestPerSecondRay = DefaultAPRBips.annualBipsToRayPerSecond();
+    }
+
+    function _getVaultAddress(address factory, bytes32 salt) internal view returns (address) {
+      return address(uint160(uint256(keccak256(abi.encodePacked(bytes1(0xff), factory, salt, VaultInitCodeHash)))));
     }
 
     function writeTokenBalance(address who, address token, uint256 amt) internal {
@@ -62,14 +77,23 @@ contract VaultFactoryTest is Test {
         wmvf = new WMVaultFactory(address(wmp));
 
         vm.prank(wintermute);
-        uint saltDAI = 1;
-        wmvf.deployVault(address(DAI), 100_000e18, 500, 90, bytes32(saltDAI));
+        bytes32 saltDAI = bytes32(uint256(1));
+        address returnedVaultAddress = wmvf.deployVault(address(DAI), DefaultMaximumSupply, DefaultAPRBips, DefaultCollateralizationRatio, saltDAI);
+        wmDAI = WMVault(returnedVaultAddress);
+
+        // Verify vault was deployed to correct address
+        assertEq(_getVaultAddress(address(wmvf), saltDAI), returnedVaultAddress);
+        // Verify factory computes correct address
+        assertEq(wmvf.computeVaultAddress(saltDAI), returnedVaultAddress);
 
         address wmrAddr = wmvf.vaultRegistryAddress();
         wmr = WMRegistry(wmrAddr);
 
         address[] memory regVaults = wmr.listVaults();
-        wmDAI = WMVault(regVaults[0]);
+
+        // Verify registry pushes vault address
+        assertEq(regVaults.length, 1);
+        assertEq(regVaults[0], returnedVaultAddress);
 
         writeTokenBalance(wlUser, address(DAI), 100_000 * 1e18);
         writeTokenBalance(nonwlUser, address(DAI), 100_000 * 1e18);
@@ -82,19 +106,22 @@ contract VaultFactoryTest is Test {
     }
 
     function test_VaultCreated() public {
-        
-        IERC20Metadata wmDAImd = IERC20Metadata(address(wmDAI));
-
-        string memory vaultName = wmDAImd.name();
-
-        // TODO: this fails, produces gibberish, seems to be a string memory problem with what's passed in
-        console.log(vaultName);
-        
-        assertTrue(
-            keccak256(abi.encodePacked(vaultName)) 
-            == keccak256(abi.encodePacked("Wintermute Dai Stablecoin"))
-        );
+        assertEq(wmDAI.name(), "Wintermute Dai Stablecoin");
+        assertEq(wmDAI.symbol(), "wmtDAI");
        
+        assertEq(wmDAI.asset(), address(DAI));
+        assertEq(wmDAI.totalSupply(), 0);
+        assertEq(wmDAI.maxTotalSupply(), DefaultMaximumSupply);
+        (
+          int256 annualInterestBips,
+          uint256 scaledTotalSupply,
+          uint256 scaleFactor,
+          uint256 lastInterestAccruedTimestamp
+        ) = wmDAI.getState();
+        assertEq(annualInterestBips, DefaultAPRBips);
+        assertEq(scaledTotalSupply, 0);
+        assertEq(scaleFactor, RayOne);
+        assertEq(lastInterestAccruedTimestamp, block.timestamp);
     }
 
     function test_PermissionsGranted() public {
@@ -121,8 +148,9 @@ contract VaultFactoryTest is Test {
         vm.prank(wlUser);
         wmDAI.deposit(50_000e18, wlUser);
         uint startBalance = wmDAI.balanceOf(wlUser);
-        console.log(startBalance);
+
         warpOneYear();
+        uint256 interest = uint256(startBalance.rayMul(DefaultInterestPerSecondRay * SecondsIn365Days));
         
         // The deposit of 1e18 is what ACTUALLY adjusts the scale factor, the balanceOf call just projects it
         /**
@@ -135,7 +163,7 @@ contract VaultFactoryTest is Test {
         **/
 
         uint endBalance = wmDAI.balanceOf(wlUser);
-        console.log(endBalance);
+        assertEq(endBalance, startBalance + interest);
 
         vm.prank(wlUser);
         wmDAI.withdraw(1e18, wlUser);
@@ -147,7 +175,7 @@ contract VaultFactoryTest is Test {
         
         // TODO: re-represent maxCollateralToWithdraw in terms of amounts deposited, not availableCapacity
         uint availableCollateral = wmDAI.maxCollateralToWithdraw();
-        console.log(availableCollateral); 
+        console2.log(availableCollateral); 
 
         vm.prank(wlUser);
         wmDAI.deposit(50_000e18, wlUser);
@@ -155,7 +183,7 @@ contract VaultFactoryTest is Test {
         // TODO: confirm that the amount of collateral available to withdraw is now 90% of what was deposited
         // TODO: scale this with scaleFactor so it's not represented in the base asset
         uint availableCollateral2 = wmDAI.maxCollateralToWithdraw();
-        console.log(availableCollateral2);
+        console2.log(availableCollateral2);
         assertTrue(true);
     }
 
