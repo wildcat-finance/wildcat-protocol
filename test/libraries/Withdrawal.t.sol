002246defb818bff530d05616a7a370ce9f801c8
d1ll0n <dillon@wildcat.finance>
2023-08-31 17:37:09 -0700

    rm tests for removed fns

--- test/libraries/Withdrawal.t.sol
+++ test/libraries/Withdrawal.t.sol
@@ -10,92 +10,7 @@ contract WithdrawalTest is Test {
 	event WithdrawalBatchCreated(uint256 expiry);
 	event WithdrawalQueued(uint256 expiry, address account, uint256 scaledAmount);
 
-	function _addWithdrawalRequest(
-		VaultState memory state,
-		address account,
-		uint104 scaledAmount,
-		uint256 withdrawalBatchDuration
-	) external returns (VaultState memory) {
-		WithdrawalLib.addWithdrawalRequest(
-			_withdrawalData,
-			state,
-			account,
-			scaledAmount,
-			withdrawalBatchDuration
-		);
-		return state;
-	}
-
-	function test_addWithdrawalRequest(
-		uint32 pendingWithdrawalExpiry,
-		address account,
-		uint104 scaledAmount,
-		// uint104 scaledTotalPendingWithdrawals,
-		uint32 withdrawalBatchDuration
-	) external {
-		withdrawalBatchDuration = uint32(
-			bound(withdrawalBatchDuration, 1, type(uint32).max - block.timestamp)
-		);
-		VaultState memory state;
-		pendingWithdrawalExpiry = uint32(bound(pendingWithdrawalExpiry, 0, uint32(block.timestamp)));
-		state.pendingWithdrawalExpiry = pendingWithdrawalExpiry;
-		if (pendingWithdrawalExpiry == 0) {
-			vm.expectEmit();
-			emit WithdrawalBatchCreated(block.timestamp + withdrawalBatchDuration);
-		}
-		uint32 newExpiry = uint32(
-			pendingWithdrawalExpiry == 0
-				? block.timestamp + withdrawalBatchDuration
-				: pendingWithdrawalExpiry
-		);
-		vm.expectEmit();
-		emit WithdrawalQueued(uint256(newExpiry), address(account), uint256(scaledAmount));
-		state = this._addWithdrawalRequest(state, account, scaledAmount, withdrawalBatchDuration);
-		assertEq(_withdrawalData.accountStatuses[newExpiry][account].scaledAmount, scaledAmount);
-		assertEq(_withdrawalData.batches[newExpiry].scaledTotalAmount, scaledAmount);
-		assertEq(state.scaledPendingWithdrawals, scaledAmount);
-
-		assertEq(state.pendingWithdrawalExpiry, newExpiry);
-	}
-
-	function test_withdrawAvailable(
-		address account,
-		uint256 expiry,
-		uint256 scaledWithdrawalAmount,
-		uint256 scaledTotalBatchAmount,
-		uint256 previousNormalizedAmountWithdrawn,
-		uint256 normalizedAmountPaid,
-		uint256 reservedAssets
-	) external {
-		expiry = bound(expiry, 0, type(uint32).max);
-		scaledTotalBatchAmount = bound(scaledTotalBatchAmount, 1, type(uint104).max);
-		scaledWithdrawalAmount = bound(scaledWithdrawalAmount, 1, scaledTotalBatchAmount);
-		reservedAssets = bound(reservedAssets, 1, type(uint128).max);
-		normalizedAmountPaid = bound(normalizedAmountPaid, 1, reservedAssets);
-		uint256 newTotal = (normalizedAmountPaid * scaledWithdrawalAmount) / scaledTotalBatchAmount;
-		previousNormalizedAmountWithdrawn = bound(previousNormalizedAmountWithdrawn, 0, newTotal);
-
-		_withdrawalData.batches[uint32(expiry)].scaledTotalAmount = uint104(scaledTotalBatchAmount);
-		_withdrawalData.batches[uint32(expiry)].normalizedAmountPaid = uint128(normalizedAmountPaid);
-		_withdrawalData.accountStatuses[expiry][account].scaledAmount = uint104(scaledWithdrawalAmount);
-		_withdrawalData.accountStatuses[expiry][account].normalizedAmountWithdrawn = uint128(
-			previousNormalizedAmountWithdrawn
-		);
-		VaultState memory state;
-		state.reservedAssets = uint128(reservedAssets);
-		uint256 expectedOutput = newTotal - previousNormalizedAmountWithdrawn;
-		assertEq(
-			WithdrawalLib.withdrawAvailable(_withdrawalData, state, account, uint32(expiry)),
-			expectedOutput
-		);
-		assertEq(state.reservedAssets, reservedAssets - expectedOutput);
-		assertEq(
-			_withdrawalData.accountStatuses[expiry][account].normalizedAmountWithdrawn,
-			previousNormalizedAmountWithdrawn + expectedOutput
-		);
-	}
-
-	function test_availableLiquidityForBatch(
+	function test_availableLiquidityForPendingBatch(
 		uint128 totalAssets,
 		uint104 scaledTotalPendingWithdrawals,
 		uint104 scaledBatchAmount,
@@ -118,6 +33,6 @@ contract WithdrawalTest is Test {
 			uint256(accruedProtocolFees) +
 			state.normalizeAmount(scaledTotalPendingWithdrawals - scaledBatchAmount);
 		uint256 expected = totalAssets > totalReserved ? totalAssets - totalReserved : 0;
-		assertEq(WithdrawalLib.availableLiquidityForBatch(batch, state, totalAssets), expected);
+		assertEq(WithdrawalLib.availableLiquidityForPendingBatch(batch, state, totalAssets), expected);
 	}
 }
