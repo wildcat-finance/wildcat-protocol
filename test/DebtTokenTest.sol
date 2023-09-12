89d3eace7effb7d5dfbd48c3b50392f1e73439f1
d1ll0n <dillon@wildcat.finance>
2023-09-12 17:37:16 +0100

    fix erc20 tests

--- test/DebtTokenTest.sol
+++ test/DebtTokenTest.sol
@@ -5,6 +5,7 @@ import { MockERC20 } from 'solmate/test/utils/mocks/MockERC20.sol';
 import 'src/WildcatVaultFactory.sol';
 import 'src/WildcatVaultController.sol';
 import './helpers/BaseERC20Test.sol';
+import './helpers/MockController.sol';
 import './shared/TestConstants.sol';
 
 bytes32 constant DaiSalt = bytes32(uint256(1));
@@ -24,9 +25,6 @@ contract DebtTokenTest is BaseERC20Test {
 
 	function _minAmount() internal override returns (uint256 min) {
 		min = divUp(WildcatMarket(address(token)).scaleFactor(), RAY);
-		// if (min * RAY < WildcatVaultToken(address(token)).scaleFactor()) {
-		// min += 1;
-		// }
 	}
 
 	function divUp(uint256 x, uint256 d) internal pure returns (uint256 z) {
@@ -44,7 +42,11 @@ contract DebtTokenTest is BaseERC20Test {
 
 	function setUp() public override {
 		factory = new WildcatVaultFactory();
-		controller = new WildcatVaultController(address(0xfee), address(factory));
+		MockController mockController = new MockController(feeRecipient, address(factory));
+
+		mockController.toggleParameterChecks();
+		mockController.authorizeAll();
+		controller = mockController;
 		asset = new MockERC20('Token', 'TKN', 18);
 
 		VaultParameters memory vaultParameters = VaultParameters({
@@ -59,7 +61,7 @@ contract DebtTokenTest is BaseERC20Test {
 			protocolFeeBips: DefaultProtocolFeeBips,
 			annualInterestBips: 10000,
 			delinquencyFeeBips: DefaultDelinquencyFee,
-			withdrawalBatchDuration: DefaultWithdrawalBatchDuration,
+			withdrawalBatchDuration: 0,
 			delinquencyGracePeriod: DefaultGracePeriod,
 			liquidityCoverageRatio: DefaultLiquidityCoverage
 		});
@@ -71,21 +73,11 @@ contract DebtTokenTest is BaseERC20Test {
 		// assertEq(WildcatMarket(address(token)).scaleFactor(), 2e27);
 	}
 
-	function test1234() external {
-		testTransferFrom(
-			0x0000000000000000000000000000000000000000,
-			324810486040414809761794,
-			5664740236411625697083734255443362985816946254289
-		);
-	}
-
-	function _assertTokenAmountEq(uint256 expected, uint256 actual) internal virtual override {
-		assertEq(expected, actual);
-	}
+	// function _assertTokenAmountEq(uint256 expected, uint256 actual) internal virtual override {
+	// 	assertEq(expected, actual);
+	// }
 
 	function _mint(address to, uint256 amount) internal override {
-		controller.authorizeLender(to);
-		controller.authorizeLender(address(this));
 		require(amount <= _maxAmount(), 'amount too large');
 		vm.startPrank(to);
 		asset.mint(to, amount);
@@ -98,8 +90,8 @@ contract DebtTokenTest is BaseERC20Test {
 	}
 
 	function _burn(address from, uint256 amount) internal override {
-		revert('todo');
-		// vm.prank(from);
-		// WildcatMarket(address(token)).withdraw(amount, address(this));
+		vm.prank(from);
+		WildcatMarket(address(token)).queueWithdrawal(amount);
+		WildcatMarket(address(token)).executeWithdrawal(from, uint32(block.timestamp));
 	}
 }
