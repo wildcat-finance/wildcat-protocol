03fefeb228c233460190747c06f34ce88f328200
d1ll0n <dillon@wildcat.finance>
2023-09-12 17:39:42 +0100

    market contract tests

---
+++ test/market/WildcatMarket.t.sol
@@ -0,0 +1,220 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.20;
+
+import '../BaseVaultTest.sol';
+import 'src/interfaces/IVaultEventsAndErrors.sol';
+import 'src/libraries/MathUtils.sol';
+import 'src/libraries/SafeCastLib.sol';
+import 'src/libraries/VaultState.sol';
+import 'solady/utils/SafeTransferLib.sol';
+
+contract WildcatMarketTest is BaseVaultTest {
+	using stdStorage for StdStorage;
+	// using WadRayMath for uint256;
+	using MathUtils for int256;
+	using MathUtils for uint256;
+
+	// ===================================================================== //
+	//                             updateState()                             //
+	// ===================================================================== //
+
+	function test_updateState() external {
+		_deposit(alice, 1e18);
+		fastForward(365 days);
+		VaultState memory state = pendingState();
+		updateState(state);
+		vault.updateState();
+		assertEq(vault.previousState(), state);
+	}
+
+	function test_updateState_NoChange() external {
+		_deposit(alice, 1e18);
+		VaultState memory state = pendingState();
+		bytes32 stateHash = keccak256(abi.encode(state));
+		vault.updateState();
+		assertEq(keccak256(abi.encode(vault.previousState())), stateHash);
+    assertEq(keccak256(abi.encode(vault.currentState())), stateHash);
+	}
+
+	function test_updateState_HasPendingExpiredBatch() external {
+    parameters.annualInterestBips = 3650;
+    setUp();
+    _deposit(alice, 1e18);
+    _requestWithdrawal(alice, 1e18);
+    uint32 expiry = previousState.pendingWithdrawalExpiry;
+    fastForward(1 days);
+    VaultState memory state = pendingState();
+    vm.expectEmit(address(vault));
+    emit ScaleFactorUpdated(1.001e27, 1e24, 0, 0);
+    vm.expectEmit(address(vault));
+    emit WithdrawalBatchExpired(
+			expiry,
+			1e18,
+			1e18,
+			1e18
+		);
+    vm.expectEmit(address(vault));
+    emit WithdrawalBatchClosed(expiry);
+    vm.expectEmit(address(vault));
+    emit StateUpdated(1.001e27, false);
+    vault.updateState();
+  }
+
+	function test_updateState_HasPendingExpiredBatch_SameBlock() external {
+    parameters.withdrawalBatchDuration = 0;
+    setUpContracts(true);
+    setUp();
+    _deposit(alice, 1e18);
+    _requestWithdrawal(alice, 1e18);
+    VaultState memory state = pendingState();
+    vm.expectEmit(address(vault));
+    emit WithdrawalBatchExpired(
+			block.timestamp,
+			1e18,
+			1e18,
+			1e18
+		);
+    vm.expectEmit(address(vault));
+    emit WithdrawalBatchClosed(block.timestamp);
+    vm.expectEmit(address(vault));
+    emit StateUpdated(1e27, false);
+    vault.updateState();
+  }
+
+	// ===================================================================== //
+	//                         depositUpTo(uint256)                          //
+	// ===================================================================== //
+
+	function test_depositUpTo() external asAccount(alice) {
+		_deposit(alice, 50_000e18);
+		assertEq(vault.totalSupply(), 50_000e18);
+		assertEq(vault.balanceOf(alice), 50_000e18);
+	}
+
+	function test_depositUpTo(uint256 amount) external asAccount(alice) {
+		amount = bound(amount, 1, DefaultMaximumSupply);
+		vault.depositUpTo(amount);
+	}
+
+	function test_depositUpTo_ApprovedOnController() public asAccount(bob) {
+		_authorizeLender(bob);
+		vm.expectEmit(address(vault));
+		emit AuthorizationStatusUpdated(bob, AuthRole.DepositAndWithdraw);
+		vault.depositUpTo(1e18);
+		assertEq(uint(vault.getAccountRole(bob)), uint(AuthRole.DepositAndWithdraw));
+	}
+
+	function test_depositUpTo_NullMintAmount() external asAccount(alice) {
+		vm.expectRevert(IVaultEventsAndErrors.NullMintAmount.selector);
+		vault.depositUpTo(0);
+	}
+
+	function testDepositUpTo_MaxSupplyExceeded() public asAccount(bob) {
+		_authorizeLender(bob);
+		asset.transfer(address(1), type(uint128).max);
+		asset.mint(bob, DefaultMaximumSupply);
+		asset.approve(address(vault), DefaultMaximumSupply);
+		vault.depositUpTo(DefaultMaximumSupply - 1);
+		vault.depositUpTo(2);
+		assertEq(vault.balanceOf(bob), DefaultMaximumSupply);
+		assertEq(asset.balanceOf(bob), 0);
+	}
+
+	function testDepositUpTo_NotApprovedLender() public asAccount(bob) {
+		asset.mint(bob, 1e18);
+		asset.approve(address(vault), 1e18);
+		vm.expectRevert(IVaultEventsAndErrors.NotApprovedLender.selector);
+		vault.depositUpTo(1e18);
+	}
+
+	function testDepositUpTo_TransferFail() public asAccount(alice) {
+		asset.approve(address(vault), 0);
+		vm.expectRevert(SafeTransferLib.TransferFromFailed.selector);
+		vault.depositUpTo(50_000e18);
+	}
+
+	// ===================================================================== //
+	//                           deposit(uint256)                            //
+	// ===================================================================== //
+
+	function test_deposit(uint256 amount) external asAccount(alice) {
+		amount = bound(amount, 1, DefaultMaximumSupply);
+		vault.deposit(amount);
+	}
+
+	function testDeposit_NotApprovedLender() public asAccount(bob) {
+		vm.expectRevert(IVaultEventsAndErrors.NotApprovedLender.selector);
+		vault.deposit(1e18);
+	}
+
+	function testDeposit_MaxSupplyExceeded() public asAccount(alice) {
+		vault.deposit(DefaultMaximumSupply - 1);
+		vm.expectRevert(IVaultEventsAndErrors.MaxSupplyExceeded.selector);
+		vault.deposit(2);
+	}
+
+	// ===================================================================== //
+	//                             collectFees()                             //
+	// ===================================================================== //
+
+	function test_collectFees_NoFeesAccrued() external {
+		vm.expectRevert(IVaultEventsAndErrors.NullFeeAmount.selector);
+		vault.collectFees();
+	}
+
+	function test_collectFees() external {
+		_deposit(alice, 1e18);
+		fastForward(365 days);
+		vm.expectEmit(address(asset));
+		emit Transfer(address(vault), feeRecipient, 1e16);
+		vm.expectEmit(address(vault));
+		emit FeesCollected(1e16);
+		vault.collectFees();
+	}
+
+	function test_collectFees_InsufficientCoverageForFeeWithdrawal() external {
+		_deposit(alice, 1e18);
+		fastForward(1);
+		asset.burn(address(vault), 1e18);
+		vm.expectRevert(IVaultEventsAndErrors.InsufficientCoverageForFeeWithdrawal.selector);
+		vault.collectFees();
+	}
+
+	// ===================================================================== //
+	//                            borrow(uint256)                            //
+	// ===================================================================== //
+
+	function test_borrow(uint256 amount) external {
+		uint256 availableCollateral = vault.borrowableAssets();
+		assertEq(availableCollateral, 0, 'borrowable should be 0');
+
+		vm.prank(alice);
+		vault.depositUpTo(50_000e18);
+		assertEq(vault.borrowableAssets(), 40_000e18, 'borrowable should be 40k');
+		vm.prank(borrower);
+		vault.borrow(40_000e18);
+		assertEq(asset.balanceOf(borrower), 40_000e18);
+	}
+
+	function test_borrow_BorrowAmountTooHigh() external {
+		vm.prank(alice);
+		vault.depositUpTo(50_000e18);
+
+		vm.startPrank(borrower);
+		vm.expectRevert(IVaultEventsAndErrors.BorrowAmountTooHigh.selector);
+		vault.borrow(40_000e18 + 1);
+	}
+
+	// ===================================================================== //
+	//                             closeVault()                              //
+	// ===================================================================== //
+
+	function test_closeVault() external asAccount(address(controller)) {
+		vault.closeVault();
+	}
+
+	function test_closeVault_NotController() external {
+		vm.expectRevert(IVaultEventsAndErrors.NotController.selector);
+		vault.closeVault();
+	}
+}
---
+++ test/market/WildcatMarketBase.t.sol
@@ -0,0 +1,170 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.20;
+
+import '../BaseVaultTest.sol';
+
+contract WildcatMarketBaseTest is BaseVaultTest {
+	// ===================================================================== //
+	//                          coverageLiquidity()                          //
+	// ===================================================================== //
+
+	function test_coverageLiquidity() external {
+		vault.coverageLiquidity();
+	}
+
+	// ===================================================================== //
+	//                             scaleFactor()                             //
+	// ===================================================================== //
+
+	function test_scaleFactor() external {
+		assertEq(vault.scaleFactor(), 1e27, 'scaleFactor should be 1 ray');
+		fastForward(365 days);
+		assertEq(vault.scaleFactor(), 1.1e27, 'scaleFactor should grow by 10% from APR');
+		// updateState(pendingState());
+		// Deposit one token
+		_deposit(alice, 1e18);
+		// Borrow 80% of vault assets
+		_borrow(8e17);
+		assertEq(vault.currentState().isDelinquent, false);
+		// Withdraw 100% of deposits
+		_requestWithdrawal(alice, 1e18);
+		assertEq(vault.scaleFactor(), 1.1e27);
+		// Fast forward to delinquency grace period
+		fastForward(2000);
+		VaultState memory state = previousState;
+		uint256 scaleFactorAtGracePeriodExpiry = uint(1.1e27) +
+			MathUtils.rayMul(
+				1.1e27,
+				FeeMath.calculateLinearInterestFromBips(parameters.annualInterestBips, 2_000)
+			);
+		assertEq(vault.scaleFactor(), scaleFactorAtGracePeriodExpiry);
+		// uint256 dayOneInterest = FeeMath.calculateLinearInterestFromBips(
+		// 	1000,
+		// 	86_400
+		// );
+		// uint256 scaleFactorDayOne = 1.1e27 + MathUtils.rayMul(1.1e27, dayOneInterest);
+		// uint256 scaleFactor = scaleFactorDayOne +
+		// 	MathUtils.rayMul(scaleFactorDayOne, FeeMath.calculateLinearInterestFromBips(2000, 364 days));
+
+		// assertEq(
+		// 	vault.scaleFactor(),
+		// 	scaleFactor,
+		// 	'scaleFactor should grow by 20% with delinquency fees'
+		// );
+	}
+
+	// ===================================================================== //
+	//                             totalAssets()                             //
+	// ===================================================================== //
+
+	function test_totalAssets() external {
+		vault.totalAssets();
+	}
+
+	// ===================================================================== //
+	//                          borrowableAssets()                           //
+	// ===================================================================== //
+
+	function test_borrowableAssets() external {
+		assertEq(vault.borrowableAssets(), 0, 'borrowable should be 0');
+
+		_deposit(alice, 50_000e18);
+		assertEq(vault.borrowableAssets(), 40_000e18, 'borrowable should be 40k');
+		// vault.borrowableAssets();
+	}
+
+	// ===================================================================== //
+	//                         accruedProtocolFees()                         //
+	// ===================================================================== //
+
+	function test_accruedProtocolFees() external {
+		vault.accruedProtocolFees();
+	}
+
+	// ===================================================================== //
+	//                            previousState()                            //
+	// ===================================================================== //
+
+	function test_previousState() external {
+		vault.previousState();
+	}
+
+	// ===================================================================== //
+	//                            currentState()                             //
+	// ===================================================================== //
+
+	function test_currentState() external {
+		vault.currentState();
+	}
+
+	// ===================================================================== //
+	//                          scaledTotalSupply()                          //
+	// ===================================================================== //
+
+	function test_scaledTotalSupply() external {
+		vault.scaledTotalSupply();
+	}
+
+	// ===================================================================== //
+	//                       scaledBalanceOf(address)                        //
+	// ===================================================================== //
+
+	function test_scaledBalanceOf(address account) external {
+		vault.scaledBalanceOf(account);
+	}
+
+	function test_scaledBalanceOf() external {
+		address account;
+		vault.scaledBalanceOf(account);
+	}
+
+	// ===================================================================== //
+	//                        effectiveBorrowerAPR()                         //
+	// ===================================================================== //
+
+	function test_effectiveBorrowerAPR() external {
+		assertEq(vault.effectiveBorrowerAPR(), 1.1e26);
+		_deposit(alice, 1e18);
+		_borrow(8e17);
+		_requestWithdrawal(alice, 1e18);
+		assertEq(vault.effectiveBorrowerAPR(), 1.1e26);
+		fastForward(2_001);
+		assertEq(vault.effectiveBorrowerAPR(), 2.1e26);
+	}
+
+	// ===================================================================== //
+	//                         effectiveLenderAPR()                          //
+	// ===================================================================== //
+
+	function test_effectiveLenderAPR() external {
+		assertEq(vault.effectiveLenderAPR(), 1e26);
+		_deposit(alice, 1e18);
+		_borrow(8e17);
+		_requestWithdrawal(alice, 1e18);
+		assertEq(vault.effectiveLenderAPR(), 1e26);
+		fastForward(2_001);
+		assertEq(vault.effectiveLenderAPR(), 2e26);
+	}
+
+	// ===================================================================== //
+	//                      withdrawableProtocolFees()                       //
+	// ===================================================================== //
+
+	function test_withdrawableProtocolFees() external {
+		assertEq(vault.withdrawableProtocolFees(), 0);
+		_deposit(alice, 1e18);
+		fastForward(365 days);
+		assertEq(vault.withdrawableProtocolFees(), 1e16);
+	}
+
+	function test_withdrawableProtocolFees_LessReservedAssets() external {
+		assertEq(vault.withdrawableProtocolFees(), 0);
+		_deposit(alice, 1e18);
+		_borrow(8e17);
+		_requestWithdrawal(alice, 1e18);
+		fastForward(365 days);
+		assertEq(vault.withdrawableProtocolFees(), 0);
+		asset.mint(address(vault), 8e17 + 1);
+		assertEq(vault.withdrawableProtocolFees(), 1);
+	}
+}
