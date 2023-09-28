3e0d7af1094346011fa88aa0a84ece05e8db54dd
d1ll0n <dillon@wildcat.finance>
2023-09-28 12:45:11 -0400

    fix transfer to wrong address

--- src/market/WildcatMarketWithdrawals.sol
+++ src/market/WildcatMarketWithdrawals.sol
@@ -138,7 +138,7 @@ contract WildcatMarketWithdrawals is WildcatMarketBase {
         borrower,
         address(asset)
       );
-      asset.safeTransfer(accountAddress, normalizedAmountWithdrawn);
+      asset.safeTransfer(escrow, normalizedAmountWithdrawn);
       emit SanctionedAccountWithdrawalSentToEscrow(
         accountAddress,
         escrow,
