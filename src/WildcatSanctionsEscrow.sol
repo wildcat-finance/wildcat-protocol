5554249fed188d75aee778228d5809ed8890262a
d1ll0n <dillon@wildcat.finance>
2023-10-10 18:36:36 -0400

    inherit IWildcatSanctionsEscrow

--- src/WildcatSanctionsEscrow.sol
+++ src/WildcatSanctionsEscrow.sol
@@ -5,19 +5,11 @@ import { IERC20 } from './interfaces/IERC20.sol';
 import { IChainalysisSanctionsList } from './interfaces/IChainalysisSanctionsList.sol';
 import { SanctionsList } from './libraries/Chainalysis.sol';
 import { WildcatSanctionsSentinel } from './WildcatSanctionsSentinel.sol';
+import { IWildcatSanctionsEscrow } from './interfaces/IWildcatSanctionsEscrow.sol';
 
-contract WildcatSanctionsEscrow {
-  event EscrowReleased(
-    address indexed caller,
-    address indexed account,
-    address indexed asset,
-    uint256 amount
-  );
-
-  error CanNotReleaseEscrow();
-
-  address public immutable borrower;
-  address public immutable account;
+contract WildcatSanctionsEscrow is IWildcatSanctionsEscrow {
+  address public immutable override borrower;
+  address public immutable override account;
 
   IChainalysisSanctionsList internal constant chainalysisSanctionsList = SanctionsList;
   address internal immutable asset;
@@ -26,19 +18,19 @@ contract WildcatSanctionsEscrow {
     (borrower, account, asset) = WildcatSanctionsSentinel(msg.sender).tmpVaultParams();
   }
 
-  function balance() public view returns (uint256) {
+  function balance() public view override returns (uint256) {
     return IERC20(asset).balanceOf(address(this));
   }
 
-  function canReleaseEscrow() public view returns (bool) {
+  function canReleaseEscrow() public view override returns (bool) {
     return !chainalysisSanctionsList.isSanctioned(account);
   }
 
-  function escrowedAsset() public view returns (address, uint256) {
+  function escrowedAsset() public view override returns (address, uint256) {
     return (asset, balance());
   }
 
-  function releaseEscrow() public {
+  function releaseEscrow() public override {
     if (msg.sender != borrower && !canReleaseEscrow()) revert CanNotReleaseEscrow();
 
     uint256 amount = balance();
