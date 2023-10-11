048170b1dd070dce9b54ef7ac596c2191cbb8d73
d1ll0n <dillon@wildcat.finance>
2023-10-11 10:46:58 -0400

    add sentinel fn

--- src/interfaces/IWildcatSanctionsEscrow.sol
+++ src/interfaces/IWildcatSanctionsEscrow.sol
@@ -3,13 +3,14 @@ pragma solidity >=0.8.20;
 
 interface IWildcatSanctionsEscrow {
   event EscrowReleased(
-    address indexed caller,
     address indexed account,
     address indexed asset,
     uint256 amount
   );
 
   error CanNotReleaseEscrow();
+
+  function sentinel() external view returns (address);
 
   function borrower() external view returns (address);
 
