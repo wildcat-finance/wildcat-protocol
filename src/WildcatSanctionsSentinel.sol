8dd45300078a521a42246940718e5dd8f68d27f8
d1ll0n <dillon@wildcat.finance>
2023-10-11 10:49:11 -0400

    use sanctions override instead of requiring borrower to call specific escrow contract

--- src/WildcatSanctionsSentinel.sol
+++ src/WildcatSanctionsSentinel.sol
@@ -3,52 +3,70 @@ pragma solidity >=0.8.20;
 
 import { IChainalysisSanctionsList } from './interfaces/IChainalysisSanctionsList.sol';
 import { IWildcatArchController } from './interfaces/IWildcatArchController.sol';
+import { IWildcatSanctionsSentinel } from './interfaces/IWildcatSanctionsSentinel.sol';
 import { SanctionsList } from './libraries/Chainalysis.sol';
 import { WildcatSanctionsEscrow } from './WildcatSanctionsEscrow.sol';
 
-contract WildcatSanctionsSentinel {
-  event NewSanctionsEscrow(
-    address indexed borrower,
-    address indexed account,
-    address indexed asset
-  );
-
-  error NotRegisteredVault();
-
-  struct TmpVaultParams {
-    address borrower;
-    address account;
-    address asset;
-  }
-
-  bytes32 public constant WildcatSanctionsEscrowInitcodeHash =
+contract WildcatSanctionsSentinel is IWildcatSanctionsSentinel {
+  bytes32 public constant override WildcatSanctionsEscrowInitcodeHash =
     keccak256(type(WildcatSanctionsEscrow).creationCode);
 
-  IChainalysisSanctionsList public immutable chainalysisSanctionsList;
+  address public immutable override chainalysisSanctionsList;
 
-  IWildcatArchController public immutable archController;
+  address public immutable override archController;
 
-  TmpVaultParams public tmpVaultParams;
+  TmpEscrowParams public override tmpEscrowParams;
+
+  mapping(address borrower => mapping(address account => bool sanctionOverride))
+    public
+    override sanctionOverrides;
 
   constructor(address _archController, address _chainalysisSanctionsList) {
-    archController = IWildcatArchController(_archController);
-    chainalysisSanctionsList = IChainalysisSanctionsList(_chainalysisSanctionsList);
-    _resetTmpVaultParams();
+    archController = _archController;
+    chainalysisSanctionsList = _chainalysisSanctionsList;
+    _resetTmpEscrowParams();
   }
 
-  function _resetTmpVaultParams() internal {
-    tmpVaultParams = TmpVaultParams(address(1), address(1), address(1));
+  function _resetTmpEscrowParams() internal {
+    tmpEscrowParams = TmpEscrowParams(address(1), address(1), address(1));
   }
 
-  function isSanctioned(address account) public view returns (bool) {
-    return chainalysisSanctionsList.isSanctioned(account);
+  /**
+   * @dev Returns boolean indicating whether `account` is sanctioned
+   *      on Chainalysis and that status has not been overridden by
+   *      `borrower`.
+   */
+  function isSanctioned(address borrower, address account) public view override returns (bool) {
+    return
+      !sanctionOverrides[borrower][account] &&
+      IChainalysisSanctionsList(chainalysisSanctionsList).isSanctioned(account);
   }
 
+  /**
+   * @dev Overrides the sanction status of `account` for `borrower`.
+   */
+  function overrideSanction(address account) public override {
+    sanctionOverrides[msg.sender][account] = true;
+    emit SanctionOverride(msg.sender, account);
+  }
+
+  /**
+   * @dev Removes the sanction override of `account` for `borrower`.
+   */
+  function removeSanctionOverride(address account) public override {
+    sanctionOverrides[msg.sender][account] = false;
+    emit SanctionOverrideRemoved(msg.sender, account);
+  }
+
+  /**
+   * @dev Calculate the create2 escrow address for the combination
+   *      of `borrower`, `account`, and `asset`.
+   */
   function getEscrowAddress(
     address borrower,
     address account,
     address asset
-  ) public view returns (address escrowAddress) {
+  ) public view override returns (address escrowAddress) {
     return
       address(
         uint160(
@@ -66,22 +84,37 @@ contract WildcatSanctionsSentinel {
       );
   }
 
+  /**
+   * @dev Creates a new WildcatSanctionsEscrow contract for `borrower`,
+   *      `account`, and `asset` or returns the existing escrow contract
+   *      if one already exists.
+   *
+   *      The escrow contract is added to the set of sanction override
+   *      addresses for `borrower` so that it can not be blocked.
+   */
   function createEscrow(
     address borrower,
     address account,
     address asset
-  ) public returns (address escrowContract) {
-    if (!archController.isRegisteredVault(msg.sender)) revert NotRegisteredVault();
+  ) public override returns (address escrowContract) {
+    if (!IWildcatArchController(archController).isRegisteredVault(msg.sender)) {
+      revert NotRegisteredVault();
+    }
 
     escrowContract = getEscrowAddress(borrower, account, asset);
 
     if (escrowContract.codehash != bytes32(0)) return escrowContract;
 
-    tmpVaultParams = TmpVaultParams(borrower, account, asset);
+    tmpEscrowParams = TmpEscrowParams(borrower, account, asset);
 
     new WildcatSanctionsEscrow{ salt: keccak256(abi.encode(borrower, account, asset)) }();
 
     emit NewSanctionsEscrow(borrower, account, asset);
-    _resetTmpVaultParams();
+
+    sanctionOverrides[borrower][escrowContract] = true;
+
+    emit SanctionOverride(borrower, escrowContract);
+    
+    _resetTmpEscrowParams();
   }
 }
--- src/interfaces/IWildcatSanctionsSentinel.sol
+++ src/interfaces/IWildcatSanctionsSentinel.sol
@@ -2,9 +2,21 @@
 pragma solidity >=0.8.20;
 
 interface IWildcatSanctionsSentinel {
-  event NewSanctionsEscrow(address borrower, address account, address asset);
+  event NewSanctionsEscrow(address indexed borrower, address indexed account, address indexed asset);
+
+  event SanctionOverride(address indexed borrower, address indexed account);
+
+  event SanctionOverrideRemoved(address indexed borrower, address indexed account);
 
   error NotRegisteredVault();
+
+  struct TmpEscrowParams {
+    address borrower;
+    address account;
+    address asset;
+  }
+
+  function WildcatSanctionsEscrowInitcodeHash() external pure returns (bytes32);
 
   // Returns immutable sanctions list contract
   function chainalysisSanctionsList() external view returns (address);
@@ -12,8 +24,23 @@ interface IWildcatSanctionsSentinel {
   // Returns immutable arch-controller
   function archController() external view returns (address);
 
+  // Returns temporary escrow params
+  function tmpEscrowParams()
+    external
+    view
+    returns (address borrower, address account, address asset);
+
   // Returns result of `chainalysisSanctionsList().isSanctioned(account)`
-  function isSanctioned(address account) external view returns (bool);
+  // if borrower has not overridden the status of `account`
+  function isSanctioned(address borrower, address account) external view returns (bool);
+
+  // Returns boolean indicating whether `borrower` has overridden the
+  // sanction status of `account`
+  function sanctionOverrides(address borrower, address account) external view returns (bool);
+
+  function overrideSanction(address account) external;
+
+  function removeSanctionOverride(address account) external;
 
   // Returns create2 address of sanctions escrow contract for
   // combination of `account,borrower,asset`
