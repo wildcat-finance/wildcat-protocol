cfa58067f606217d40687e7e06de3c1344ac4e3c
d1ll0n <dillon@indexed.finance>
2023-11-30 03:55:37 -0600

    remove unused file

--- src/libraries/Chainalysis.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-import '../interfaces/IChainalysisSanctionsList.sol';
-
-IChainalysisSanctionsList constant SanctionsList = IChainalysisSanctionsList(
-  0x40C57923924B5c5c5455c48D93317139ADDaC8fb
-);
